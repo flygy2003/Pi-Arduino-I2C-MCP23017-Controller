87eecabe2d1c3e81cdd7c88b755c452900ddea28
flygy2003 <neekonsu@gmail.com>
2018-06-24 19:44:26 -0700

    alexandria: less comments

--- app.js
+++ app.js
@@ -49,12 +49,12 @@ function read(addrA, pin) {
   }
 }
 
-// db.ref("VirtualDB").on("child_changed", (data) => {
-//   if(data.key+1 > 24) {
-//     // write(((data.key+1 > 16) ? false : true), data.key+1, data.val().state)
-//   } else {
-//     console.log("index ${data.key} will be written to by station 1")
-//   }
-//   console.log(data.val())
-//   console.log(data.key)
-// })
+db.ref("VirtualDB").on("child_changed", (data) => {
+  if(data.key+1 > 24) {
+    // write(((data.key+1 > 16) ? false : true), data.key+1, data.val().state)
+  } else {
+    console.log("index ${data.key} will be written to by station 1")
+  }
+  console.log(data.val())
+  console.log(data.key)
+})
